04e4eeba00bab0bb1c6347447e2e57f097ba371f
Esmail Elmoussel <elmoussel12@gmail.com>
2024-02-16 01:23:28 +0200

    feat(server): create simple todo functions!

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,19 @@
-const myName: string = "Esmail Elmoussel!";
+import { Todo } from "./types/todo.types";
 
-console.log(myName);
+const todos: Todo[] = [];
+
+const createTodo = (text: string): Todo => {
+  const todo = { id: todos.length + 1, text };
+
+  todos.push(todo);
+
+  return todo;
+};
+
+const getTodos = (): Todo[] => {
+  return todos;
+};
+
+createTodo("Watch Match!");
+
+console.log(getTodos());
